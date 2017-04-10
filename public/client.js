4b255be56e5a3a5aaf44558ed94083e73e6f03f4
kramarmm <kramar.mariya@mail.ru>
2017-04-10 09:46:34 +0300

    Added extension checker

--- public/client.js
+++ public/client.js
@@ -1,7 +1,34 @@
+var getImageArr = function() {
+    $.ajax({
+        url: "/pictures",
+        method: "POST",
+        cache: false,
+        contentType: false,
+        processData: false
+    }).then(function(res) {
+        console.log(res);
+    });
+}
+
+$( document ).ready(getImageArr);
+
 $('#uploadForm').submit(function(e) {
+    var imageName = $('input[type="file"]')[0].files[0].name.split("."),
+        imageExt = imageName[imageName.length -1];
+
     e.preventDefault();
+    
+    // checking file extension
+    var validExt = ["jpg", "jpeg", "png"];
+    var checker = false;
 
+    validExt.forEach(function(ext) {
+            if( imageExt === ext) checker = true;
+        });
 
+    if( !checker ) return;
+
+    // if extension valid send request
     $.ajax({
         url: "/upload",
         method: "POST",
@@ -11,7 +38,10 @@ $('#uploadForm').submit(function(e) {
         data: new FormData(jQuery('#uploadForm')[0])
     }).then(function(res) {
         console.log(res);
-    })
-
+    });
+    
+    getImageArr();
     return false;
+
 });
+
